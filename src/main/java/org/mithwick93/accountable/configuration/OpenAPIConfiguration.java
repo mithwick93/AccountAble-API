5738ea3ea2d6ab19a3c26ba37d8d04d56c4073fa
mithwick93 <mithwick93@gmail.com>
2024-11-10 19:19:44 +0100

    configure security configs
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

--- src/main/java/org/mithwick93/accountable/configuration/OpenAPIConfiguration.java
+++ src/main/java/org/mithwick93/accountable/configuration/OpenAPIConfiguration.java
@@ -1,8 +1,14 @@
 package org.mithwick93.accountable.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +17,16 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfiguration {
     @Bean
     public OpenAPI customOpenAPI(
-            @Value("${application-title}") String title,
-            @Value("${application-description}") String description,
-            @Value("${application-version}") String version
+            @Value("${application.title}") String title,
+            @Value("${application.description}") String description,
+            @Value("${application.version}") String version
     ) {
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(In.HEADER)
+                .name("Authorization");
         return new OpenAPI()
                 .info(
                         new Info()
@@ -27,7 +39,14 @@ public class OpenAPIConfiguration {
                                                 .name("Apache 2.0")
                                                 .url("https://springdoc.org/v2/")
                                 )
-                );
-
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", bearerAuth)
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .paths(new Paths()
+                        .addPathItem("/api/user/register", new PathItem())
+                        .addPathItem("/api/user/login", new PathItem()));
     }
 }
---
+++ src/main/java/org/mithwick93/accountable/configuration/SecurityConfiguration.java
@@ -0,0 +1,100 @@
+package org.mithwick93.accountable.configuration;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.mithwick93.accountable.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Autowired
+    private RsaKeyConfigProperties rsaKeyConfigProperties;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtDecoder jwtDecoder = jwtDecoder();
+        http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/user/register",
+                                "/api/user/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder)))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtDecoder), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeyConfigProperties.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey
+                .Builder(rsaKeyConfigProperties.publicKey())
+                .privateKey(rsaKeyConfigProperties.privateKey())
+                .build();
+        return new NimbusJwtEncoder(new ImmutableJWKSet<>(new JWKSet(jwk)));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @ConfigurationProperties(prefix = "rsa")
+    record RsaKeyConfigProperties(
+            RSAPublicKey publicKey,
+            RSAPrivateKey privateKey
+    ) {
+    }
+}

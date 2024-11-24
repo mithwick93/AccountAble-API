25ec07ce67432b35f00b1ce54da3618136a20f34
mithwick93 <mithwick93@gmail.com>
2024-11-24 18:53:58 +0100

    drop null fields in transaction response
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

--- src/main/java/org/mithwick93/accountable/controller/dto/response/TransactionResponse.java
+++ src/main/java/org/mithwick93/accountable/controller/dto/response/TransactionResponse.java
@@ -1,5 +1,7 @@
 package org.mithwick93.accountable.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -7,20 +9,39 @@ import java.util.List;
 
 public record TransactionResponse(
         long id,
+
         String name,
+
         String description,
+
         String type,
+
         Integer categoryId,
+
         BigDecimal amount,
+
         String currency,
+
         LocalDate date,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Integer fromAssetId,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Integer toAssetId,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Integer fromPaymentSystemId,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         Integer toPaymentSystemId,
+
         int userId,
+
         List<SharedTransactionResponse> sharedTransactions,
+
         LocalDateTime created,
+
         LocalDateTime modified
 ) {
 

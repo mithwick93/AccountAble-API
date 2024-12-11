293cb7ebfd2c68cba882aea541f712da3335b711
mithwick93 <mithwick93@gmail.com>
2024-12-11 23:16:11 +0100

    allow local frontend
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

--- src/main/java/org/mithwick93/accountable/configuration/SecurityConfiguration.java
+++ src/main/java/org/mithwick93/accountable/configuration/SecurityConfiguration.java
@@ -98,7 +98,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:3000/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);

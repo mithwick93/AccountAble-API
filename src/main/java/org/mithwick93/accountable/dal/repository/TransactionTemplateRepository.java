a21d1107196674e8c4e2f31253339e9b0cfc9d84
mithwick93 <mithwick93@gmail.com>
2025-02-18 23:20:49 +0100

    add transaction template repository
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

---
+++ src/main/java/org/mithwick93/accountable/dal/repository/TransactionTemplateRepository.java
@@ -0,0 +1,17 @@
+package org.mithwick93.accountable.dal.repository;
+
+import org.mithwick93.accountable.model.TransactionTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransactionTemplateRepository extends JpaRepository<TransactionTemplate, Integer> {
+
+    List<TransactionTemplate> findAllByUserId(int userId);
+
+    Optional<TransactionTemplate> findByIdAndUserId(long id, int userId);
+
+}

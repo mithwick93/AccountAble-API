87ee462b858de6052846111ca5ac6824404dd34a
mithwick93 <mithwick93@gmail.com>
2025-06-20 20:37:56 +0200

    add UserRegistrationRepository with method to check existence by username or email
    Signed-off-by: mithwick93 <mithwick93@gmail.com>

---
+++ src/main/java/org/mithwick93/accountable/dal/repository/specification/UserRegistrationRepository.java
@@ -0,0 +1,12 @@
+package org.mithwick93.accountable.dal.repository.specification;
+
+import org.mithwick93.accountable.model.UserRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRegistrationRepository extends JpaRepository<UserRegistration, Integer> {
+
+    boolean existsByUsernameOrEmail(String username, String email);
+
+}
